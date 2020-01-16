2c8cfaca6f9c32fa4254ff1a6207eade3cfaaa21
linux <christian.colonna@studio.unibo.it>
2020-01-16 18:04:24 +0100

    offset are returned but code is not clean. It is better do the correction as last operation after RDF is produced

--- src/rstmarcutree/classes.py
+++ src/rstmarcutree/classes.py
@@ -8,6 +8,198 @@ from Queue import Queue
 
 from discoursegraphs import read_distree
 
+
+class RSTNode(object):
+    """ Marcu RST tree node """
+    def __init__(self, index, status, relation, schema, parent_pointer, child_pointers, text, parent, childs, promotion_set):
+        self.index = index
+        self.status = status # ROOT, NUCLEUS or SATELLITE
+        self.relation = relation # Relations or LEAF
+        self.schema = schema # MULTINUCLEAR or DEPENDENT
+        self.parent_pointer = parent_pointer # parent
+        self.child_pointers = child_pointers # list of childs
+        self.text = text # the text spanned by the node
+        self.parent = parent
+        self.childs = childs
+        self.promotion_set = promotion_set # the promotion or saliency set
+    
+    def get_nucleus(self):
+        """ It returns the nucleus of the node,
+            a list of nucleus if the node surrounds a multinuclear relation.
+            None if the node is an edu.
+        """
+        if self.schema == 'MULTINUC':
+            return self.childs
+        elif self.relation == 'LEAF':
+            return None
+        else:
+            for child in self.childs:
+                if child.status == 'N':
+                    return child 
+
+    def get_satellite(self):
+        """ It returns the satellite of a node:
+            None if the node surrounds a multinuclear relation.
+            None if the node is an edu.
+        """
+        for child in self.childs:
+            if child.status == 'S':
+                return child
+        return None
+
+class RSTTree(object):
+    """
+    A class that represents a Marcu RST tree as it is described in paper:
+    Building Up Rhetorical Structure Trees 1996.
+    This is composed by RSTNode.
+    Root has pointer 0.
+
+    RSTTree is composed by RSTNode
+    """
+    def __init__(self, nodes):
+        def rescale_tree_to_set_parent_and_childs_and_promotion_sets(nodes):
+            """ parent and childs are set by pointers
+            """
+            def get_node_by_index(index, nodes):
+                for node in nodes:
+                    if node.index == index:
+                        return node
+        
+            def get_most_important_units(node):
+                """ Function use queue, scanning nodes, if the might be important they are added to the queue.
+                    Then one at a time are examined to see if are important.
+                    They are important relative to a node if are nucleus of the node and terminal.
+                """
+                def is_important(node):
+                    return not node.get_nucleus()
+
+                queue = Queue()
+                queue.put(node)
+
+                while not queue.empty():
+                    possible_important_node = queue.get()
+                    if is_important(possible_important_node):
+                        node.promotion_set.append(possible_important_node) # if important add to promotion set
+                    if possible_important_node.childs:
+                        # the might be important if nucleus, add them to the queue!    
+                        other_possible_important_nodes = [child for child in possible_important_node.childs if child.status == 'N']
+                        for other_possible_important_node in other_possible_important_nodes:
+                            queue.put(other_possible_important_node)
+
+            # childs and parents                 
+            for node in nodes:
+                if node.parent_pointer is not None:
+                    node.parent = get_node_by_index(node.parent_pointer, nodes)
+                if node.child_pointers:
+                    node.childs = [get_node_by_index(child_pointer, nodes) for child_pointer in node.child_pointers]
+        
+            # promotion sets
+            for node in nodes:    
+                get_most_important_units(node)
+
+        rescale_tree_to_set_parent_and_childs_and_promotion_sets(nodes)
+        self.nodes = nodes
+    
+    def get_nodes_by_relation(self, relation):
+        """ Return a list of tree nodes by relation they span
+        """
+        nodes_by_relation = []
+        for node in self.nodes:
+            if node.relation == relation:
+                nodes_by_relation.append(node)
+        return nodes_by_relation
+
+    def get_edus(self):
+        """ Return edu's of the tree
+        """
+        return self.get_nodes_by_relation("LEAF")
+
+    def get_node(self, index):
+        """ Return the node by index
+        """
+        for node in self.nodes:
+            if node.index == index:
+                return node 
+
+
+class RSTEDUAligner(object):
+    """ This object helps to align the edu text (elementary discourse unit) of RSTTree to the original
+        plain text consumed by the rst parser. This may be necessary since the parser tokenize the text
+        inserting spaces and causing disalignment.    
+    """
+    @staticmethod
+    def get_original_edu_offsets(edus, original_text):
+        """ Return the boundaries of an edu in original plain text. We process all the file in a hit.
+            This way we always know the initial boundary of the edu since it starts from zero.     
+        """
+        edu_offsets = [] # ((starts_char, end_char), index)
+        new_edus = []
+        starts_at = 0 # the position where the current edu starts 
+
+        for old_edu in edus:
+            
+            last_edu_token = old_edu.text.split()[-1]  # the terminal token of the edu
+
+            # all the candidates that can be the end of the edu
+            edu_ending_boundary_candidates = [(i + len(last_edu_token)) for i in RSTEDUAligner.find_all_occurences(last_edu_token, original_text[starts_at:])]
+            
+            # we select the candidate such as the length last_char  - starts_at is the closest to
+            # len(tokenized edu)
+            
+            winning_candidate = 0
+            for possible_terminal_char in edu_ending_boundary_candidates:
+                if abs((len(old_edu.text)) - possible_terminal_char) <= abs((len(old_edu.text)) - winning_candidate):
+                    winning_candidate = possible_terminal_char
+                ends_at = winning_candidate + starts_at
+            
+            edu_offsets.append(((starts_at, ends_at),old_edu.index))
+            new_edus.append(original_text[starts_at:ends_at])
+
+            try:
+                for token in old_edu.text.split():
+                    assert(token in original_text[starts_at:ends_at])
+            except:
+                print("There's a bug, edu not aligned")
+                print(" TODO") 
+                # TODO: if there are more tokens, check first if it belongs to the previous edu if not it
+                # belongs to next one. Adjust boundaries! Big mess! 
+                # Recursion? While?
+                exit
+
+            # move the pointer: this makes the search of ending token (n log n) to the length of the document
+            starts_at = ends_at
+        try:
+            assert(original_text == ''.join(new_edus))           
+        except:
+            print("There's a bug, length of plain text != text derived by concatenation of new edu")
+            print("To correct this you need TODO: ")
+            print(" while if ")
+            print("CONCATENATED EDUS TEXT")
+            print(''.join(new_edus))
+            print(len(''.join(new_edus)))
+            print(original_text)
+            print("ORIGINAL TEXT")
+            print(len(original_text))
+            exit           
+        return(edu_offsets)
+
+    @staticmethod
+    def read_plain_text(plain_text_path):
+        with open(plain_text_path) as fh:
+            lines = []
+            for line in fh:
+                lines.append(line)
+            return ''.join(lines)[0:-1] # we remove a final /n
+    @staticmethod
+    def find_all_occurences(p, s):
+        """ Yields all the occurences of the substring p in the string s.
+        """
+        i = s.find(p)
+        while i != -1:
+            yield i
+            i = s.find(p, i+1)
+
+
 class RSTTreeHelper(object):
     """ A class that scan and analyze a DGParentedTree representation of an RST tree.
         This class is necessary while the nltk representation used by discoursegraphs
@@ -163,118 +355,3 @@ class RSTTreeHelper(object):
             -> DGParentedTree : (A discoursegraphs subclass of nltk ParentedTree) discourse_tree
         """
         return read_distree(file_path).tree
-
-
-class RSTNode(object):
-    """ Marcu RST tree node """
-    def __init__(self, index, status, relation, schema, parent_pointer, child_pointers, text, parent, childs, promotion_set):
-        self.index = index
-        self.status = status # ROOT, NUCLEUS or SATELLITE
-        self.relation = relation # Relations or LEAF
-        self.schema = schema # MULTINUCLEAR or DEPENDENT
-        self.parent_pointer = parent_pointer # parent
-        self.child_pointers = child_pointers # list of childs
-        self.text = text # the text spanned by the node
-        self.parent = parent
-        self.childs = childs
-        self.promotion_set = promotion_set # the promotion or saliency set
-    
-    def get_nucleus(self):
-        """ It returns the nucleus of the node,
-            a list of nucleus if the node surrounds a multinuclear relation.
-            None if the node is an edu.
-        """
-        if self.schema == 'MULTINUC':
-            return self.childs
-        elif self.relation == 'LEAF':
-            return None
-        else:
-            for child in self.childs:
-                if child.status == 'N':
-                    return child 
-
-    def get_satellite(self):
-        """ It returns the satellite of a node:
-            None if the node surrounds a multinuclear relation.
-            None if the node is an edu.
-        """
-        for child in self.childs:
-            if child.status == 'S':
-                return child
-        return None
-
-class RSTTree(object):
-    """
-    A class that represents a Marcu RST tree as it is described in paper:
-    Building Up Rhetorical Structure Trees 1996.
-    This is composed by RSTNode.
-    Root has pointer 0.
-
-    RSTTree is composed by RSTNode
-    """
-    def __init__(self, nodes):
-        def rescale_tree_to_set_parent_and_childs_and_promotion_sets(nodes):
-            """ parent and childs are set by pointers
-            """
-            def get_node_by_index(index, nodes):
-                for node in nodes:
-                    if node.index == index:
-                        return node
-        
-            def get_most_important_units(node):
-                """ Function use queue, scanning nodes, if the might be important they are added to the queue.
-                    Then one at a time are examined to see if are important.
-                    They are important relative to a node if are nucleus of the node and terminal.
-                """
-                def is_important(node):
-                    return not node.get_nucleus()
-
-                queue = Queue()
-                queue.put(node)
-
-                while not queue.empty():
-                    possible_important_node = queue.get()
-                    if is_important(possible_important_node):
-                        node.promotion_set.append(possible_important_node) # if important add to promotion set
-                    if possible_important_node.childs:
-                        # the might be important if nucleus, add them to the queue!    
-                        other_possible_important_nodes = [child for child in possible_important_node.childs if child.status == 'N']
-                        for other_possible_important_node in other_possible_important_nodes:
-                            queue.put(other_possible_important_node)
-
-            # childs and parents                 
-            for node in nodes:
-                if node.parent_pointer is not None:
-                    print(node.index, node.parent_pointer)
-                    node.parent = get_node_by_index(node.parent_pointer, nodes)
-                if node.child_pointers:
-                    node.childs = [get_node_by_index(child_pointer, nodes) for child_pointer in node.child_pointers]
-        
-            # promotion sets
-            for node in nodes:    
-                get_most_important_units(node)
-
-        rescale_tree_to_set_parent_and_childs_and_promotion_sets(nodes)
-        self.nodes = nodes
-    
-    def get_nodes_by_relation(self, relation):
-        """ Return a list of tree nodes by relation they span
-        """
-        nodes_by_relation = []
-        for node in self.nodes:
-            if node.relation == relation:
-                nodes_by_relation.append(node)
-        return nodes_by_relation
-
-    def get_edus(self):
-        """ Return edu's of the tree
-        """
-        return self.get_nodes_by_relation("LEAF")
-
-    def get_node(self, index):
-        """ Return the node by index
-        """
-        for node in self.nodes:
-            if node.index == index:
-                return node 
-
--- src/rstmarcutree/prova.py
+++ src/rstmarcutree/prova.py
@@ -1,15 +1,18 @@
 from rstmarcutree import load_tree
 from discoursegraphs import read_distree
+from classes import RSTEDUAligner
 
-tree = load_tree('text.dis')
+tree = load_tree('t.dis')
 
 # tree = read_distree('text.dis')
 # for node in tree.tree.subtrees():
 #     print node
 
 
-print("index  status  relation  schema   p_pointer   c_pointers   parent    childs ")
 
-for node in tree.nodes:
-    print(node, node.index, node.status, node.relation, node.schema, node.parent_pointer,node.child_pointers,node.parent,node.childs)
-    print("")
+original_text = RSTEDUAligner.read_plain_text('t')
+original_offsets = RSTEDUAligner.get_original_edu_offsets(tree.get_edus(), original_text)
+
+for offset in original_offsets:
+    print original_text[offset[0][0]:offset[0][1]]
+    print offset[0]
